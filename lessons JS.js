2778c9bc90fd09909b15a3f00ca9e07e7f94cfa3
Igor <mr.greatanderson@gmail.com>
2020-06-01 15:58:23 +0300

    first commit

---
+++ lessons JS.js
@@ -0,0 +1,39 @@
+
+"use script";
+// 6.Класификация типов даных
+let number = 4;
+console.log(number);
+
+const obj = {
+    name: "John",
+    age: 24,
+    isMarried: false
+};
+console.log(obj.name);
+
+let arr = [24, 'orange', 'sokol,'];
+console.log(arr[2]);
+
+// 7.Простое общение с пользователем.
+/*alert('Hello');*/
+/*const answer = prompt("Вам есть 18?","18");
+console.log(typeof (answer));*/
+const answer = [];
+//answer[0] = prompt('Как ваше имя?', '');
+//answer[1] = prompt('Как ваше фамилия?', '');
+//answer[2] = prompt('Сколько вам лет?', '');
+
+console.log(typeof(answer));
+
+// 9. Операторы в  js
+
+let incr = 10;
+let decr = 10;
+
+incr++;
+decr--;
+console.log(incr);
+console.log(decr);
+
+
+
