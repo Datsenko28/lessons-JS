a87593ca8a5e15196bc566b3d5d895750ece6395
Igor <mr.greatanderson@gmail.com>
2020-06-04 15:16:59 +0300

    add script file(cycles and conditions)

--- lessons JS.js
+++ lessons JS.js
@@ -1,6 +1,7 @@
 
 "use script";
 // 6.Класификация типов даных
+/*
 let number = 4;
 console.log(number);
 
@@ -13,20 +14,22 @@ console.log(obj.name);
 
 let arr = [24, 'orange', 'sokol,'];
 console.log(arr[2]);
+*/
 
 // 7.Простое общение с пользователем.
 /*alert('Hello');*/
 /*const answer = prompt("Вам есть 18?","18");
-console.log(typeof (answer));*/
+console.log(typeof (answer));
 const answer = [];
 //answer[0] = prompt('Как ваше имя?', '');
 //answer[1] = prompt('Как ваше фамилия?', '');
 //answer[2] = prompt('Сколько вам лет?', '');
 
 console.log(typeof(answer));
+*/
 
 // 9. Операторы в  js
-
+/*
 let incr = 10;
 let decr = 10;
 
@@ -34,6 +37,35 @@ incr++;
 decr--;
 console.log(incr);
 console.log(decr);
+*/
+
+
+// 14. Циклы
+
+//let num = 50;
+/*
+while (num < 55) {
+    console.log(num);
+    num++;
+} */
+/*
+do {
+    console.log(num);
+    num++;
+}
+while (num < 55);
+*/
+/*for (let  i = 1; i < 8; i++) {
+    console.log(num);
+    num++;   
+}
+*/
+
+let i = 1;
+while (i < 10) {
+    console.log(i);
+    i++;
+}
 
 
 
